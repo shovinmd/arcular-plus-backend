ca876f6f2f0f5867d98475c886016e1dddb498e5
Shovin Michel David <shovinmicheldavid1285@gmail.com>
2025-09-23 03:08:05 +0530

    update doc chat

--- server.js
+++ server.js
@@ -98,6 +98,7 @@ if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
       'https://arcular-plus-staffs.vercel.app',
       'https://arcular-plus-backend-man-65aq.vercel.app',
       'https://arcular-plus-backend-man.vercel.app',
+      'https://arcular-pluse-a-unified-healthcare-peach.vercel.app',
       'http://localhost:55853',
       'http://localhost:3000',
       'http://localhost:59123',
